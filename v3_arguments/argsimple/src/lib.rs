a0b09bd5425f5334daa398768a2a0383db75a791
Maximiliano Usich <maximilianou@gmail.com>
2025-03-25 06:21:08 +0100

    feat: add test

--- v3_arguments/argsimple/src/lib.rs
+++ v3_arguments/argsimple/src/lib.rs
@@ -6,3 +6,20 @@ pub fn parameter(){
     dbg!(args);
     println!(" -- parameter() -- ");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn case_sensitive(){
+        let query = "duck";
+        let contents = "Rust: safe, fast, productive. Pick three. Duct tape.";
+        assert_eq!(vec!["save, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive(){
+        let query = "rUst";
+        let contents = "Rust: safe, fast, productive. Pick three. Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
+}

407f1efac5920328fb5d4b0386d2fc36685f620d
Maximiliano Usich <maximilianou@gmail.com>
2025-04-13 21:00:48 +0200

    doc: ip static for the subnet tailscale

--- v5_tdd_sendmail/simplemail/src/lib.rs
+++ v5_tdd_sendmail/simplemail/src/lib.rs
@@ -1,3 +1,11 @@
+
+use std::error::Error;
+
+use lettre::{
+    message::header::ContentType, transport::smtp::authentication::Credentials, Message,
+    SmtpTransport, Transport,
+};
+
 pub struct SimpleEmail {
   pub from: String,
   pub reply_to: String,
@@ -20,15 +28,31 @@ pub struct SimpleMailConfig {
   pub mailer: SimpleMailer,
 }
 
-pub fn simple_send() -> Result<(), Box<dyn Err>>{
+impl SimpleMailConfig {
+    pub fn build(args: &[String] ) -> Result<SimpleMailConfig, &'static str> {
+
+        Ok( SimpleMailConfig {} ) 
+    }
+}
+
+pub fn simple_send(config : SimpleMailConfig) -> Result<(), Box<dyn Error>>{
     Ok(())
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_simple_send(){
-
-        simple_send();
+    fn test_simple_send_config(){
+        let config = SimpleMailConfig::build(
+          ["admin@simpledoers.work",
+          "admin@simpledoers.work",
+          "dev@simpledoers.work",
+          ContentType::TEXT_PLAIN,
+          "Messaggio TDD in rust simplemail subject",
+          "Messaggio TDD in rust simplemail body"
+          ]
+        );
+        assert_eq!((), simple_send(config.expect("expect some config")).expect("The email is set ok"));
     }
-}
+ }
